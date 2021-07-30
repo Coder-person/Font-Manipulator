d62540418f55ee47a30a9b4573c23c4ebb066048
Coder-person <73622307+Coder-person@users.noreply.github.com>
2021-07-29 18:57:16 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function preload(){
+
+}
+
+function setup(){
+    canvas = createCanvas(400,400);
+    canvas.position(700,100);
+    video = createCapture(VIDEO);
+    video.size(400,400);
+}
+
+function draw(){
+    
+}
